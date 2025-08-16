fa051260b20b0afa0790a72eced41f66ae25dea9
Manisha <banerjeemanisha763@gmail.com>
2025-08-16 18:32:50 +0530

    edited pic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,15 @@ import { AboutSection } from "../components/AboutSection";
 import { ContactSection } from "../components/ContactSection";
 import { Footer } from "../components/Footer";
 import { HeroSection } from "../components/HeroSection";
+import {ThemeToggle} from "../components/ThemeToggle";
 import { Navbar } from "../components/Navbar";
 import { ProjectSection } from "../components/ProjectSection";
 import { SkillsSection } from "../components/SkillsSection";
-import { ThemeToggle } from "../components/ThemeToggle"
+
 import {StartBackground} from "@/components/StartBackground"
 export const Home=() =>{
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
-   {<ThemeToggle/>}
+ <ThemeToggle/>
    <StartBackground/>
    <Navbar/>
    <main>
