cc3b376b3e0fcc7903d4412e97e3dcd8a20b89d4
Manisha <banerjeemanisha763@gmail.com>
2025-10-27 10:50:29 +0530

    Updated project with minor changes

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -32,7 +32,7 @@ export const AboutSection = () => {
                 Get In Touch
               </a>
               <a
-                href="/projects/CV.pdf"
+                href="/projects/MY CV.pdf"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duartion-300"
               >
                 Download CV
